80b3376e0bcdb5d1ff5a904ddcf3b11c8be484f2
josue121291 <99739712+josue121291@users.noreply.github.com>
2022-03-01 20:40:42 -0500

    Add files via upload

---
+++ Ejercicios de variables/variables1.py
@@ -0,0 +1,8 @@
+mensaje = "Fundamentos de programación"
+mensaje = 'Fundamentos de programación'
+n = 365
+pi =  3.1415
+
+print(mensaje)
+print(n)
+print(pi)
---
+++ Ejercicios de variables/variables2.py
@@ -0,0 +1,14 @@
+
+# Creo una variable de nombre dia
+dia = "martes"
+
+# Imprimo el tipo de variable
+print(type(dia))
+
+dia = 20
+# print(dia)
+# Para sabe el tipo de variable utilizamos la función type
+print(type(dia))
+
+dia = 20.5
+print(type(dia))
